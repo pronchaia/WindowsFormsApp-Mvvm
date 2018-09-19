51ff35c749f5b1311940e629361122d8125a8cef
PRONCHAI AKSONNITI <zpronchai.a@pttdigital.com>
2018-09-19 15:34:18 +0700

    Event Step 1

--- WindowsFormsApp.Mvvm/Form1.Designer.cs
+++ WindowsFormsApp.Mvvm/Form1.Designer.cs
@@ -28,13 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.btnClickMe = new System.Windows.Forms.Button();
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(151, 48);
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(273, 26);
+            this.txtMsg.TabIndex = 0;
+            // 
+            // btnClickMe
+            // 
+            this.btnClickMe.Location = new System.Drawing.Point(231, 106);
+            this.btnClickMe.Name = "btnClickMe";
+            this.btnClickMe.Size = new System.Drawing.Size(96, 38);
+            this.btnClickMe.TabIndex = 1;
+            this.btnClickMe.Text = "Click Me";
+            this.btnClickMe.UseVisualStyleBackColor = true;
+            this.btnClickMe.Click += new System.EventHandler(this.btnClickMe_Click);
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.Location = new System.Drawing.Point(251, 156);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(51, 20);
+            this.lblMsg.TabIndex = 2;
+            this.lblMsg.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(588, 298);
+            this.Controls.Add(this.lblMsg);
+            this.Controls.Add(this.btnClickMe);
+            this.Controls.Add(this.txtMsg);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.Button btnClickMe;
+        private System.Windows.Forms.Label lblMsg;
     }
 }
 
--- WindowsFormsApp.Mvvm/Form1.cs
+++ WindowsFormsApp.Mvvm/Form1.cs
@@ -16,5 +16,18 @@ namespace WindowsFormsApp.Mvvm
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClickMe_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtMsg.Text))
+            {
+                lblMsg.Text = txtMsg.Text;
+            }
+        }
     }
 }
